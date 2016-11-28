fe9026259216706ad35ee2da264e2d9f929e9171
mohong <522916411@qq.com>
2016-11-28 13:00:11 +0800

    App.vue中ajax获取商家数据

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,22 @@
 
 <script>
   import header from './components/header/header.vue';
+  const ERR_OK = 0;
   export default {
+    data() {
+      return {
+        seller: {}
+      };
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+          response = response.body;
+          if (response.errno === ERR_OK) {
+            this.seller = response.data;
+            console.log(this.seller);
+          }
+      });
+    },
     components: {
       'v-header': header
     }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
 import goods from './components/goods/goods.vue';
 import ratings from './components/ratings/ratings.vue';
@@ -8,6 +9,7 @@ import seller from './components/seller/seller.vue';
 import './common/stylus/index.styl';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 let app = Vue.extend(App);
 
