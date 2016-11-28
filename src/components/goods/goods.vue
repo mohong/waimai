270038a42d7963c4064f0d429fb87c5bab3768c9
mohong <522916411@qq.com>
2016-11-28 11:11:34 +0800

    tab路由实现

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>i am goods</div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,11 @@
+<template>
+  i am ratings
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
