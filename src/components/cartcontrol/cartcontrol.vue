f320baf36fa97187af079ab59d08e8dd290a79d9
mohong <522916411@qq.com>
2016-12-01 10:58:17 +0800

    cartcontrol组件与购物车实现联动

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart" transition="movie">
-      <span class="inner"></span>
+    <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart" transition="move">
+      <span class="inner icon-remove_circle_outline"></span>
     </div>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addCart"></div>
@@ -43,15 +43,29 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .cartcontrol
     font-size: 0
-    .cart-decrease, .cart-add
+    .cart-decrease
       display: inline-block
       padding: 6px
-      line-height: 24px
-      font-size: 24px
-      color: rgb(0, 160, 220)
+      transition: all 0.4s linear
+      &.move-transition
+        opacity: 1
+        transform: translate3D(0, 0, 0)
+        .inner
+          display: inline-block
+          line-height: 24px
+          font-size: 24px
+          color: rgb(0, 160, 220)
+          transition: all 0.4s linear
+          transform: rotate(0)
+      &.move-enter,&.move-leave
+        opacity: 0
+        transform: translate3D(24px, 0, 0)
+        .inner
+          transform: rotate(180deg)
     .cart-count
       display: inline-block
       vertical-align: top
+      width: 12px
       padding-top: 6px
       line-height: 24px
       text-align: center
@@ -59,5 +73,8 @@
       color: rgb(147, 153, 159)
     .cart-add
       display: inline-block
-
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0, 160, 220)
 </style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -37,7 +37,7 @@
         </li>
       </ul>
     </div>
-    <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+    <shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 
@@ -71,6 +71,17 @@
           }
         }
         return 0;
+      },
+      selectFoods() {
+        let foods = [];
+        this.goods.forEach((good) => {
+          good.foods.forEach((food) => {
+            if (food.count) {
+              foods.push(food);
+            }
+          });
+        });
+        return foods;
       }
     },
     created() {
