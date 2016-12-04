e15556daa03047950d1d2e7f416084acfb8bf063
mohong <522916411@qq.com>
2016-12-04 11:45:41 +0800

    评论overview完成

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -7,18 +7,38 @@
           <div class="title">综合评分</div>
           <div class="rank">高于周边商家{{seller.rankRate}}%</div>
         </div>
-        <div class="overview-right"></div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import star from '../star/star';
+
   export default {
     props: {
       seller: {
         type: Object
       }
+    },
+    components: {
+      star
     }
   };
 </script>
@@ -40,6 +60,9 @@
        width: 137px
        border-right: 1px solid rgba(7, 17, 27, 0.1)
        text-align: center
+       @media only screen and (max-width: 320px)
+         flex: 0 0 120px
+         width: 120px
        .score
          margin-bottom: 6px
          line-height: 28px
@@ -56,5 +79,36 @@
          color: rgb(147, 153, 159)
      .overview-right
        flex: 1
-       padding-left: 24px
+       padding: 6px 0 6px 24px
+       @media only screen and (max-width: 320px)
+         padding: 6px 0 6px 6px
+       .score-wrapper
+         margin-bottom: 8px
+         font-size: 0
+         .title
+           display: inline-block
+           line-height: 18px
+           vertical-align: top
+           font-size: 12px
+           color: rgb(7, 17, 27)
+         .star
+           display: inline-block
+           margin: 0 12px
+           vertical-align: top
+         .score
+           display: inline-block
+           line-height: 18px
+           vertical-align: top
+           font-size: 12px
+           color: rgb(255, 153, 0)
+       .delivery-wrapper
+         font-size: 0
+         .title
+           line-height: 18px
+           font-size: 12px
+           color: rgb(7, 17, 27)
+         .delivery
+           margin-left: 12px
+           font-size: 12px
+           color: rgb(147, 153, 159)
 </style>
