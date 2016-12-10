669d2daf3a4f383ac5c618fbbcf1dbb7dd0a7b3d
mohong <522916411@qq.com>
2016-12-10 11:31:04 +0800

    编写url解析函数

--- src/App.vue
+++ src/App.vue
@@ -18,20 +18,27 @@
 </template>
 
 <script>
+  import {urlParse} from './common/js/util';
   import header from './components/header/header.vue';
   const ERR_OK = 0;
   export default {
     data() {
       return {
-        seller: {}
+        seller: {
+            id: (() => {
+                let queryParam = urlParse();
+                return queryParam.id;
+            })()
+        }
       };
     },
     created() {
-      this.$http.get('/api/seller').then((response) => {
+      this.$http.get('/api/seller?id=' + this.seller.id).then((response) => {
           response = response.body;
           if (response.errno === ERR_OK) {
-            this.seller = response.data;
+            this.seller = Object.assign({}, this.seller, response.data);
           }
+          console.log(this.seller.id);
       });
     },
     components: {
--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -28,6 +28,10 @@
                         </div>
                     </li>
                 </ul>
+                <div class="favorite" @click="toggleFavorite">
+                    <span class="icon-favorite" :class="{'active':favorite}"></span>
+                    <span class="text">{{favoriteText}}</span>
+                </div>
             </div>
             <split></split>
             <div class="bulletin">
@@ -75,6 +79,16 @@
                 type: Object
             }
         },
+        data() {
+            return {
+                favorite: false
+            };
+        },
+        computed: {
+            favoriteText() {
+                return this.favorite ? '已收藏' : '收藏';
+            }
+        },
         created() {
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
         },
@@ -89,6 +103,12 @@
             this._initPics();
         },
         methods: {
+            toggleFavorite(event) {
+                if (!event._constructed) {
+                    return;
+                }
+                this.favorite = !this.favorite;
+            },
             _initScroll() {
                 if (!this.scroll) {
                     this.scroll = new BScroll(this.$els.seller, {
@@ -135,6 +155,7 @@
         width: 100%
         overflow: hidden
         .overview
+            position: relative
             padding: 18px
             .title
                 margin-bottom: 8px
@@ -176,6 +197,24 @@
                         color: rgb(7, 17, 27)
                         .stress
                             font-size: 24px
+            .favorite
+                position: absolute
+                width: 50px
+                right: 11px
+                top: 18px
+                text-align: center
+                .icon-favorite
+                    display: block
+                    margin-bottom: 4px
+                    line-height: 24px
+                    font-size: 24px
+                    color: #d4d6d9
+                    &.active
+                        color: rgb(240, 20, 20)
+                .text
+                    line-height: 10px
+                    font-size: 10px
+                    color: rgb(77, 85, 93)
         .bulletin
             padding: 18px 18px 0 18px
             .title
